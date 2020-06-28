2c169222a0337f7229302daada9251ac737737f9
Oleg <smvzhgt@gmail.com>
2020-06-29 02:15:05 +0300

    refactoring - comment mock api client

--- lib/temp/mock_api_client.dart
+++ lib/temp/mock_api_client.dart
@@ -1,57 +1,72 @@
-import 'package:dartz/dartz.dart';
-import 'package:nyt_news/core/exceptions.dart';
-import 'package:nyt_news/core/models/article_response_model.dart';
-import 'package:nyt_news/core/api_client/api_client.dart';
+// import 'package:dartz/dartz.dart';
+// import 'package:nyt_news/core/exceptions.dart';
+// import 'package:nyt_news/core/models/article_response_model.dart';
+// import 'package:nyt_news/core/api_client/api_client.dart';
 
-class MockApiClientImpl implements ApiClient {
-  @override
-  Future<Either<NetworkException, List<ArticleModel>>> fetchNews() async {
-    return Future<Either<NetworkException, List<ArticleModel>>>.delayed(
-      Duration(seconds: 1),
-      () async {
-        return DataSource().getArticleModels();
-      },
-    );
-  }
-}
+// class MockApiClientImpl implements ApiClient {
+//   @override
+//   Future<Either<NetworkException, List<ArticleModel>>> fetchNews() async {
+//     return Future<Either<NetworkException, List<ArticleModel>>>.delayed(
+//       Duration(seconds: 1),
+//       () async {
+//         return DataSource().getArticleModels();
+//       },
+//     );
+//   }
 
-class DataSource {
-  Future<Either<NetworkException, List<ArticleModel>>>
-      getArticleModels() async {
-    List<ArticleModel> list = [
-      DataSource().makeArticle(),
-      DataSource().makeArticle(),
-      DataSource().makeArticle(),
-      DataSource().makeArticle(),
-      DataSource().makeArticle(),
-      DataSource().makeArticle(),
-      DataSource().makeArticle(),
-      DataSource().makeArticle(),
-      DataSource().makeArticle(),
-      DataSource().makeArticle(),
-      DataSource().makeArticle(),
-      DataSource().makeArticle()
-    ];
-    return Right(list);
-  }
+//   @override
+//   Future<Either<NetworkException, List<ArticleModel>>> fetchMostEmailedArticles() {
+//     throw UnimplementedError();
+//   }
 
-  final _abstract =
-      'The default growable list, as returned by new List() or [], keeps an internal buffer, and grows that buffer when necessary. This guarantees that a sequence of add operations will each execute in amortized constant time.';
+//   @override
+//   Future<Either<NetworkException, List<ArticleModel>>> fetchMostSharedArticles() {
+//     throw UnimplementedError();
+//   }
 
-  ArticleModel makeArticle() {
-    MediaMetadata mediaMetadata = MediaMetadata(url: '');
-    List<MediaMetadata> mediaMetadataList = [mediaMetadata];
+//   @override
+//   Future<Either<NetworkException, List<ArticleModel>>> fetchMostViewedArticles() {
+//     throw UnimplementedError();
+//   }
+// }
 
-    Media media = Media(mediaMetadata: mediaMetadataList);
-    List<Media> mediaList = [media];
+// class DataSource {
+//   Future<Either<NetworkException, List<ArticleModel>>>
+//       getArticleModels() async {
+//     List<ArticleModel> list = [
+//       DataSource().makeArticle(),
+//       DataSource().makeArticle(),
+//       DataSource().makeArticle(),
+//       DataSource().makeArticle(),
+//       DataSource().makeArticle(),
+//       DataSource().makeArticle(),
+//       DataSource().makeArticle(),
+//       DataSource().makeArticle(),
+//       DataSource().makeArticle(),
+//       DataSource().makeArticle(),
+//       DataSource().makeArticle(),
+//       DataSource().makeArticle()
+//     ];
+//     return Right(list);
+//   }
 
-    ArticleModel model = ArticleModel(
-        id: 1,
-        url: 'wwww',
-        title: 'ArticleModel title',
-        abstract: _abstract,
-        media: mediaList);
+//   final _abstract =
+//       'The default growable list, as returned by new List() or [], keeps an internal buffer, and grows that buffer when necessary. This guarantees that a sequence of add operations will each execute in amortized constant time.';
 
-    return model;
-  }
-}
+//   ArticleModel makeArticle() {
+//     MediaMetadata mediaMetadata = MediaMetadata(url: '');
+//     List<MediaMetadata> mediaMetadataList = [mediaMetadata];
+
+//     Media media = Media(mediaMetadata: mediaMetadataList);
+//     List<Media> mediaList = [media];
+
+//     ArticleModel model = ArticleModel(
+//         id: 1,
+//         url: 'wwww',
+//         title: 'ArticleModel title',
+//         abstract: _abstract,
+//         media: mediaList);
+
+//     return model;
+//   }
+// }
