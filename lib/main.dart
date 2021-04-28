7689c538914b9a11cc7095876dc461bc14deac13
Oleg <smvzhgt@gmail.com>
2021-04-28 18:33:00 +0300

    fix title

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'generated/l10n.dart';
 import 'package:nyt_news/src/scenes/bottom_navigation/presentation/pages/bottom_navigation_page.dart';
 import 'package:nyt_news/di/injection.dart' as di;
 
+
 void main() {
   di.init();
   runApp(App());
@@ -25,7 +26,7 @@ class App extends StatelessWidget {
         GlobalCupertinoLocalizations.delegate,
       ],
       supportedLocales: S.delegate.supportedLocales,
-      title: S.of(context).app_title,
+      title: "New York Times Reader",
       initialRoute: BottomNavigationPage.routeName,
       routes: {
         BottomNavigationPage.routeName: (context) =>
